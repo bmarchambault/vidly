975b4c89c150561d6e378f9416d228b50ad08998
Bobbie Archambault <bmarchambault@gmail.com>
2020-11-09 17:30:23 -0600

    modified index.js to render initial view of movies component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Movies from "./components/movies";
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'fontawesome';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+      <Movies />
   </React.StrictMode>,
   document.getElementById('root')
 );
