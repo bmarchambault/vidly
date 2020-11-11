077d30e8fcba74640f17bf22f5725d13f0761df4
Bobbie Archambault <bmarchambault@gmail.com>
2020-11-10 18:43:59 -0600

    fixed that html rendering should be in app.js and not index.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import React, { Component } from "react";
+import Movies from "./components/movies";
 
 function App() {
   return (
      <main className="container">
-       <h1>Hello World</h1>
+       {/*<h1>Hello World</h1>*/}
+         <Movies />
      </main>
   );
 }
