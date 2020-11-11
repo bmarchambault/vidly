2049e8b69e7dcf8e3274e5eaa1d88936ee7ece37
Bobbie Archambault <bmarchambault@gmail.com>
2020-11-10 18:44:46 -0600

    dynamic movie list, using bootstrap table and array mapping.

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,22 +1,36 @@
 import React, { Component } from 'react';
-
+import {getMovies} from "../services/fakeMovieService";
 class Movies extends Component {
     state = {
-        count: 0
+        count: 0,
+        movies: getMovies()
 
     };
     render() {
         return (
             <div>
+                <table className="table">
+                    <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Genre</th>
+                        <th>Stock</th>
+                        <th>Rate</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    { this.state.movies.map(movie => (
+                        <tr>
+                            <td>{movie.title}</td>
+                            <td>{movie.genre.name}</td>
+                            <td>{movie.numberInStock}</td>
+                            <td>{movie.dailyRentalRate}</td>
+                        </tr>
+                    )) }
+                    </tbody>
+                </table>
                 <span>Showing {this.formatCount()} movies in the database.</span>
-            <table>
-                <tr>
-                    <th> Title </th>
-                    <th> Genre </th>
-                    <th> Stock </th>
-                    <th> Rate </th>
-                </tr>
-            </table>
+
             </div>
         )
     }
@@ -27,3 +41,22 @@ class Movies extends Component {
 }
 
 export default Movies
+//
+// {/*    MY ATTEMPT BELOW:  */}
+// {/*<table className='table'>*/}
+// {/*    <thead>*/}
+// {/*    <tr>*/}
+// {/*        <th> Title </th>*/}
+// {/*        <th> Genre </th>*/}
+// {/*        <th> Stock </th>*/}
+// {/*        <th> Rate </th>*/}
+// {/*    </tr>*/}
+// {/*    </thead>*/}
+// {/*    <tbody>*/}
+// {/*    <tr>*/}
+// {/*        <th>*/}
+// {/*        <td> {getMovies()} </td>*/}
+// {/*        </th>*/}
+// {/*        </tr>*/}
+// {/*    </tbody>*/}
+// {/*</table>*/}
